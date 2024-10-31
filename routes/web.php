c5d6be8d4713a3da7595baa4380fa35708536cb9
Flavio Boni <flavioboni17@gmail.com>
2024-10-31 22:04:12 +0700

    Belajar Laravel 11 | 6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,10 +11,52 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Flavio Boni', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Flavio Boni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum dolorem dolores expedita suscipit non at? Placeat, est maiores ullam possimus autem perferendis obcaecati quae, aspernatur ipsam harum hic quibusdam!'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Flavio Boni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi quidem consectetur suscipit facere vero voluptas aut, praesentium quo optio, ratione dolor, officia numquam aspernatur odio rem perferendis sed autem!'
+        ]
+    ] ]);
 });
 
-Route::get('contact', function () {
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Flavio Boni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum dolorem dolores expedita suscipit non at? Placeat, est maiores ullam possimus autem perferendis obcaecati quae, aspernatur ipsam harum hic quibusdam!'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Flavio Boni',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi quidem consectetur suscipit facere vero voluptas aut, praesentium quo optio, ratione dolor, officia numquam aspernatur odio rem perferendis sed autem!'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
